a4be5b0564b70949acb25b3a1ed7e8a27417ade2
pendeldam <45753426+pendeldam@users.noreply.github.com>
2019-11-03 01:44:28 +0300

    fixed filtering

--- js/filter.js
+++ js/filter.js
@@ -1,78 +1,75 @@
 'use strict';
 (function () {
-  var activeFilters = {};
-  var mappingFilters = {
+  var filters = [];
+  var mapping = {
     'housing-type': 'type',
     'housing-price': 'price',
     'housing-rooms': 'rooms',
     'housing-guests': 'guests',
-    'wifi': 'wifi',
-    'dishwasher': 'dishwasher',
-    'parking': 'parking',
-    'washer': 'washer',
-    'elevator': 'elevator',
-    'conditioner': 'conditioner'
+  };
+
+  var convertPrice = function (item) {
+    if (item.offer.price < 10000) {
+      return 'low';
+    } else if (item.offer.price > 10000 && item.offer.price < 50000) {
+        return 'middle';
+    } else if (item.offer.price > 50000) {
+        return 'high';
+    }
+  };
+
+  var check = function (filters, item) {
+    for (var i = 0; i < filters.length; i++) {
+      console.log(filters[i].value, item.offer[filters[i].type]);
+      if (filters[i].value !== item.offer[filters[i].type]) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  var filter = function(offers) {
+    document.querySelectorAll('.map__pin:not(.map__pin--main)').forEach(function (pin) {
+      pin.remove();
+    });
+
+    if (document.querySelector('.map__card')) {
+      window.card.closeCardHandler();
+    }
+
+    window.filtering.filtered = offers.filter(function (item) {
+      console.log('check:', check(filters, item));
+      return check(filters, item);
+    });
+
+    console.log(window.filtering.filtered);
+  };
+
+  var remove = function (evt, filters) {
+
   };
 
   window.filtering = {
     add: function(evt) {
       if (event.target.name === 'features') {
-        activeFilters[mappingFilters[evt.target.value]] = evt.target.value
+        filters.push({type: evt.target.value, value: true});
         if (!evt.target.checked) {
-          delete activeFilters[mappingFilters[evt.target.value]];
+          //delete filters[evt.target.value];
         }
+      } else if (event.target.name === 'housing-rooms' || event.target.name === 'housing-guests') {
+          console.log(mapping[evt.target.name], evt.target.value);
+          filters.push({type: mapping[evt.target.name], value: +evt.target.value});
       } else {
-          activeFilters[mappingFilters[evt.target.name]] = evt.target.value;
+          filters.push({type: mapping[evt.target.name], value: evt.target.value});
       }
-
+      /*
       if (evt.target.value === 'any') {
-        delete activeFilters[mappingFilters[evt.target.name]];
+        delete filters[mapping[evt.target.name]];
       }
-
-      window.filtering.check(activeFilters);
-      console.log(activeFilters);
-    },
-    check: function(filters) {
-      document.querySelectorAll('.map__pin:not(.map__pin--main)').forEach(function (pin) {
-        pin.remove();
-      });
-
-      if (document.querySelector('.map__card')) {
-        window.card.closeCardHandler();
-      }
-
-      window.map.offers.forEach(function (item) {
-        item.rank = 0;
-        for (var value in filters) {
-          if (item.offer[value] === filters[value] && value == 'type') {
-            item.rank += 5;
-            console.log(value ,item.offer[value], filters[value], item.rank);
-          }
-          if (item.offer[value] == filters[value] && value == 'rooms') {
-            item.rank += 3;
-            console.log(value ,item.offer[value], filters[value], item.rank);
-          }
-          if (item.offer[value] == filters[value] && value == 'guests') {
-            item.rank += 2;
-            console.log(value ,item.offer[value], filters[value], item.rank);
-          }
-        }
-      });
-      
-      var sorted = window.map.offers.sort(function (left, right) {
-        return right.rank - left.rank;
-      });
-
-      var filtered = sorted.filter(function (item) {
-        if (window.map.offers[0].rank === 0) {
-          return false;
-        }
-        return item.rank === window.map.offers[0].rank;
-      });
-
-      console.log(sorted);
-      console.log(filtered);
-      window.render.pin(filtered);
+      */
+      filter(window.map.offers);
+      console.log(filters);
+      window.render.pin(window.filtering.filtered);
     }
   };
 })();
