e559cc732c00dd7a03b3dd358d4cb9d1d0c78f1d
pendeldam <45753426+pendeldam@users.noreply.github.com>
2019-11-03 03:13:07 +0300

    update filtering

--- js/filter.js
+++ js/filter.js
@@ -1,6 +1,6 @@
 'use strict';
 (function () {
-  var filters = [];
+  var filters = {};
   var mapping = {
     'housing-type': 'type',
     'housing-price': 'price',
@@ -8,20 +8,28 @@
     'housing-guests': 'guests',
   };
 
-  var convertPrice = function (item) {
-    if (item.offer.price < 10000) {
+  var convertPrice = function (price) {
+    if (price < 10000) {
       return 'low';
-    } else if (item.offer.price > 10000 && item.offer.price < 50000) {
+    } else if (price > 10000 && price < 50000) {
         return 'middle';
-    } else if (item.offer.price > 50000) {
+    } else if (price > 50000) {
         return 'high';
     }
   };
 
   var check = function (filters, item) {
-    for (var i = 0; i < filters.length; i++) {
-      console.log(filters[i].value, item.offer[filters[i].type]);
-      if (filters[i].value !== item.offer[filters[i].type]) {
+    for (var value in filters) {
+      if (value === 'price') {
+        console.log(value, filters[value], convertPrice(item.offer[value]));
+        if (filters[value] !== convertPrice(item.offer[value])) {
+          return false;
+        }
+        return true;
+      }
+
+      console.log(value, filters[value], item.offer[value]);
+      if (filters[value] !== item.offer[value]) {
         return false;
       }
     }
@@ -37,39 +45,33 @@
       window.card.closeCardHandler();
     }
 
-    window.filtering.filtered = offers.filter(function (item) {
+    var filtered = offers.filter(function (item) {
       console.log('check:', check(filters, item));
       return check(filters, item);
     });
-
-    console.log(window.filtering.filtered);
-  };
-
-  var remove = function (evt, filters) {
-
+    console.log(filtered);
+    window.render.pin(filtered);
   };
 
   window.filtering = {
     add: function(evt) {
       if (event.target.name === 'features') {
-        filters.push({type: evt.target.value, value: true});
+        filters[evt.target.value] = evt.target.value;
         if (!evt.target.checked) {
-          //delete filters[evt.target.value];
+          delete filters[evt.target.value];
         }
       } else if (event.target.name === 'housing-rooms' || event.target.name === 'housing-guests') {
-          console.log(mapping[evt.target.name], evt.target.value);
-          filters.push({type: mapping[evt.target.name], value: +evt.target.value});
+          filters[mapping[evt.target.name]] = +evt.target.value;
       } else {
-          filters.push({type: mapping[evt.target.name], value: evt.target.value});
+          filters[mapping[evt.target.name]] = evt.target.value;
       }
-      /*
+
       if (evt.target.value === 'any') {
         delete filters[mapping[evt.target.name]];
       }
-      */
+
       filter(window.map.offers);
       console.log(filters);
-      window.render.pin(window.filtering.filtered);
     }
   };
 })();
